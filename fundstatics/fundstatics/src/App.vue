7c68d6709d2a7dead49cfd6df35fa7f87b371892
dasgk <674026565@qq.com>
2020-06-16 21:54:42 +0800

    首页布局

--- fundstatics/fundstatics/src/App.vue
+++ fundstatics/fundstatics/src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
-    <el-row>
-  <el-button>默认按钮</el-button>
-  <el-button type="primary">主要按钮</el-button>
-  <el-button type="success">成功按钮</el-button>
-  <el-button type="info">信息按钮</el-button>
-  <el-button type="warning">警告按钮</el-button>
-  <el-button type="danger">危险按钮</el-button>
-</el-row>
-
-    <img src="./assets/logo.png">
+   <el-container>
+    <el-header>
+      <div>
+  <el-link  type="primary">主题基金</el-link>
+  <el-link   type="success">涨幅排名</el-link>
+  <el-link  type="warning">跌幅排名</el-link>
+  <el-link    type="danger">稳定性排名</el-link>
+  <el-link type="info">综合排名</el-link>
+</div>
+    </el-header>
+    <el-main>Main</el-main>
+   </el-container>
     <router-view/>
   </div>
 </template>
@@ -27,6 +29,46 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.el-header{
+  color:white;
+}
+
+  .el-header, .el-footer {
+
+
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+
+  .el-main {
+   # background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+
+  .el-link{
+    width: 200px;
+  }
+
+
+
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
+
 </style>
