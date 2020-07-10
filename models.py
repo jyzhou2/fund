55503af57c515e93a2385ca03a19de3ea65f8aa0
dasgk <674026565@qq.com>
2020-07-10 21:17:48 +0800

    提交估算计算代码

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from peewee import *
-database = MySQLDatabase('test', **{'charset': 'utf8', 'use_unicode': True, 'host': '47.93.76.140', 'user': 'test', 'password': 'hengda','port':3306})
+database = MySQLDatabase('test', **{'charset': 'utf8', 'use_unicode': True, 'host': '127.0.0.1', 'user': 'test', 'password': 'hdlnmp','port':3306})
 
 class JiJinGuSuan(Model):
     jjdm = CharField(null=True)
