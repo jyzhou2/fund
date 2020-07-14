b27cd648b458a916e915e8ea031b6f3e40366cd7
dasgk <674026565@qq.com>
2020-07-14 11:44:55 +0800

    提交首页布局

--- fundstatics/bootstrapvue-demo/src/components/Cards.vue
+++ fundstatics/bootstrapvue-demo/src/components/Cards.vue
@@ -3,7 +3,7 @@
 
     <b-carousel
       id="carousel-1"
-      :interval="4000"
+      :interval="2000"
       controls
       indicators
       background="#ababab"
@@ -13,18 +13,21 @@
     >
       <!-- Text slides with image -->
       <b-carousel-slide
-        caption="First slide"
-        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        img-src="https://picsum.photos/1024/480/?image=52"
+
+        img-src="http://81.70.21.205/img/data_analysize.jpg"
       ></b-carousel-slide>
 
       <!-- Slides with custom text -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54"  >
-        <h1>Hello world!</h1>
+      <b-carousel-slide
+
+              img-src="http://81.70.21.205/img/python.jpeg"  >
+
       </b-carousel-slide>
 
       <!-- Slides with image only -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"  ></b-carousel-slide>
+      <b-carousel-slide
+
+              img-src="http://81.70.21.205/img/laravel.png"  ></b-carousel-slide>
 
       <!-- Slides with img slot -->
 
