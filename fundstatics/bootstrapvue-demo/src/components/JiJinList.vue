f8a79b571de9e8c1894d4ea67bbe8c291724f15d
dasgk <674026565@qq.com>
2020-07-18 10:03:59 +0800

    修改页面

--- fundstatics/bootstrapvue-demo/src/components/JiJinList.vue
+++ fundstatics/bootstrapvue-demo/src/components/JiJinList.vue
@@ -19,30 +19,7 @@
                                     <b-form-input type="number" v-model="guimo"
                                                   placeholder="输入基金规模"></b-form-input>
                                 </b-col>
-                                <b-col>
-                                    <b-form-select v-model="week_selected" :options="week_options">
 
-
-                                    </b-form-select>
-                                </b-col>
-                                <b-col>
-                                    <b-form-select v-model="month_selected" :options="month_options">
-
-                                    </b-form-select>
-                                </b-col>
-                                <b-col>
-                                    <b-form-select v-model="three_months_selected" :options="three_months_options">
-
-                                    </b-form-select>
-                                </b-col>
-                                <b-col>
-                                    <b-form-select v-model="half_year_selected" :options="half_year_options">
-                                    </b-form-select>
-                                </b-col>
-                                <b-col>
-                                    <b-form-select v-model="year_selected" :options="year_options">
-                                    </b-form-select>
-                                </b-col>
                                 <b-col>
                                     <b-button @click="search">Search
                                     </b-button>
@@ -50,15 +27,14 @@
                             </b-row>
                         </b-container>
 
-
                         <!--  这里是搜索数结果数据 -->
-
+                        <br>
+                        <br>
                         <div class="overflow-auto">
 
 
-
-
                             <b-table
+                                    striped hover
                                     id="my-table"
                                     :items="items"
 
@@ -79,6 +55,13 @@
 
 </template>
 <style>
+    td {
+        height: 45px !important;
+    }
+
+    tr{
+        height: 45px !important;
+    }
     #tags-basic {
         height: 25px !important;
     }
@@ -115,76 +98,46 @@
 
             return {
                 value: [],
-                theme:'',
+                theme: '',
                 week_selected: null,
-                guimo: 0,
-                month_selected: null,
-                three_months_selected: null,
-                half_year_selected: null,
-                year_selected: null,
-                week_options: ['正序'],
-                month_options: ['正序'],
-                three_months_options: ['正序'],
-                half_year_options: ['正序'],
-                year_options: ['正序'],
+                guimo: 5,
+
                 perPage: 3,
                 currentPage: 1,
-                items: [
-
-
-                ]
+                items: []
             }
         },
         name: "JiJinList",
         methods: {
             //  搜索
             search() {
-                var url="";
+                var url = "";
                 url = 'http://81.70.21.205/api/fund_list?p=w&';
-                url +='guimo='+ self.guimo+"&"
-                url += 'theme='+self.theme+"&"
-                url += 'week_selected='+self.week_selected+"&"
+                url += 'guimo=' + self.guimo + "&"
+                url += 'theme=' + self.theme + "&"
+                url += 'week_selected=' + self.week_selected + "&"
 
-                url+= 'month_selected='+ self.month_selected+"&"
-                url+= 'three_months_selected='+ self.three_months_selected+"&"
-                url+= 'half_year_selected='+ self.half_year_selected+"&"
-                url+= 'year_selected='+ self.year_selected+"&"
+                url += 'month_selected=' + self.month_selected + "&"
+                url += 'three_months_selected=' + self.three_months_selected + "&"
+                url += 'half_year_selected=' + self.half_year_selected + "&"
+                url += 'year_selected=' + self.year_selected + "&"
 
                 this.axios.get(url).then((response) => {
                     var datas = response.data;
-                    datas= datas.data
-                    var i=null;
+                    datas = datas.data
+                    var i = null;
                     self.items = []
-                    for(i in datas){
+                    for (i in datas) {
                         var cur_item = datas[i]
                         self.items.push({
                             '名称': cur_item.name,
                             '规模': cur_item.guimo_number,
-                            '周水平': cur_item.one_week_level,
-                            '月水平': cur_item.one_month_level,
-                            '三月水平': cur_item.three_months_level,
-                            '半年水平': cur_item.six_months_level,
-                            '年水平': cur_item.six_months_level,
-
+                            '水平(周-月-三月-半年)': cur_item.one_week_level+"-"+cur_item.one_month_level+"-"+cur_item.three_months_level+"-"+cur_item.six_months_level,
+                            '推荐值':1
                         })
-                        console.log(i)
                     }
                 })
-                /*
-                var guimo = self.guimo
-                guimo += ''
-                var theme = self.theme
-                theme += ''
-                var week_selected = self.week_selected
-                week_selected += ''
-                var month_selected = self.month_selected
-                month_selected += ''
-                var three_months_selected = self.three_months_selected
-                three_months_selected += ''
-                var half_year_selected = self.half_year_selected
-                half_year_selected += ''
-                var year_selected = self.year_selected
-                year_selected += ''*/
+
             },
 
         }
--- fundstatics/bootstrapvue-demo/src/main.js
+++ fundstatics/bootstrapvue-demo/src/main.js
@@ -5,12 +5,54 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router.js'
 import axios from 'axios'
+import {Loading} from 'element-ui'
 import VueAxios from 'vue-axios'
+
 
 Vue.use(VueAxios, axios)
 Vue.use(BootstrapVue)
 
+let requestCount = 0;
+var loading = null;
+let timer = null;
 
+function showLoading() {
+    if (requestCount === 0) {
+        loading = Loading.service({
+            spinner: 'mobile-loading',
+            background: "rgba(0,0,0,0.8)"
+        })
+    }
+    requestCount++
+
+}
+
+function tryHideLoading() {
+    requestCount--
+    timer = setTimeout(() => {
+        if (requestCount === 0) {
+            loading.close()
+            clearTimeout(timer)
+        }
+
+    })
+}
+
+axios.interceptors.request.use(function (config) {
+        showLoading()
+        return config;
+    },
+    function (error) {
+        return Promise.reject(error)
+    })
+axios.interceptors.response.use(function (response) {
+    tryHideLoading()
+    return response
+
+}, function (error) {
+    return Promise.reject(error)
+
+})
 Vue.config.productionTip = false
 new Vue({
     router,
