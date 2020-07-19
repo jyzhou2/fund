d9e2f9370865e2ac50cf1e911cea56d298b870df
dasgk <674026565@qq.com>
2020-07-19 19:33:06 +0800

    修改页面

--- JiJinRecord/recommand.py
+++ JiJinRecord/recommand.py
@@ -9,6 +9,7 @@ class CurvePloy():
     def __init__(self,jjdm,count):
         self.jjdm = jjdm
         self.count = count
+        self.x = []
 
     def get_ploy1d(self):
         info_list = JiJinRecord.select().where(JiJinRecord.jjdm == self.jjdm).order_by(JiJinRecord.date.desc()).limit(self.count)
@@ -20,9 +21,41 @@ class CurvePloy():
             date.append(index)
             index = index + 1
         y = pd.to_numeric(y)
+        self.x = date
         z1 = np.polyfit(date, y, 2)  # 用3次多项式拟合
         p1 = np.poly1d(z1)
-        print(p1)
+        # 获得 a b c
+        c = p1[0]
+        b = p1[1]
+        a = p1[2]
+        return [a,b,c]
+
+    def get_recommand(self,a,b,c):
+        if a>0:
+            return 0
+        recommand = 0;
+        max_pos_x = 0-(b/(2*a))
+        print(max_pos_x)
+        print(self.x)
+        for cur_x in self.x:
+            if max_pos_x < cur_x:
+                recommand = recommand+1
+
+        return recommand
+
+
+
+        # 如果定点坐标小于  样本数据x坐标的最小值，则表示一直在下跌，推荐值是样本数量的个数
+
+        # 如果定点坐标 处于 样本数据x坐标的中间，则推荐值样本数据x坐标中大于max_pos_x的个数
+        # 如果顶点坐标大于 样本数据x坐标的最大值， 则表示处于上升期，推荐值是 0
+
+    def handle(self):
+        result = self.get_ploy1d()
+        a = result[0]
+        b = result[1]
+        c = result[2]
+        print( self.get_recommand(a,b,c))
 
 mode = CurvePloy('000004',7)
-mode.get_ploy1d()
+mode.handle()
