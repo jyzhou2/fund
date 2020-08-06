84db8f94dfd7234fbc190e0031de14c8a377258a
dasgk <674026565@qq.com>
2020-08-06 22:06:57 +0800

    修改路径

--- models/JiJinGuSuan.py
+++ models/JiJinGuSuan.py
@@ -12,6 +12,7 @@ class JiJinGuSuan(Model):
     six_months_level = CharField(null=True)  # 六个月水平
     gszzl = CharField(null=True)  # 六个月水平
     jijin_pic = CharField(null=True)  # 六个月水平
+    recommand = CharField(null=True)  # 六个月水平
     class Meta:
         database = database
         db_table = 'dlj_jijingusuan'
