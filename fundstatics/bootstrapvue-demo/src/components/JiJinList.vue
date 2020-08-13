26eab50a8a58af675da9cf6cee1f64fb1f5618eb
dasgk <674026565@qq.com>
2020-08-13 10:35:09 +0800

    暂时提交

--- fundstatics/bootstrapvue-demo/src/components/JiJinList.vue
+++ fundstatics/bootstrapvue-demo/src/components/JiJinList.vue
@@ -3,36 +3,37 @@
         <el-col :span="4">
 
             <el-menu
-                    :default-active="1"
+                    :default-active="default_active"
                     :active="navselected"
                     class="el-menu-vertical-demo"
                     @open="handleOpen"
+                    @select="selectItems"
                     @close="handleClose">
-                <el-menu-item index="1">
+                <el-menu-item index="1" @click="jumpto_data_analysize()">
                     <i class="el-icon-star-off"></i>
 
-                    <span slot="title" @click="jumpto_data_analysize()">基金筛选</span>
+                    <span slot="title">基金筛选</span>
                 </el-menu-item>
-                <el-menu-item index="2">
+                <el-menu-item index="2" @click="get_article_list_for_data()">
                     <i class="el-icon-s-data"></i>
-                    <span slot="title" @click="get_article_list_for_data()">数据分析</span>
+                    <span slot="title">数据分析</span>
                 </el-menu-item>
                 <el-menu-item index="3">
                     <i class="el-icon-s-platform"></i>
                     <span slot="title">服务架构</span>
                 </el-menu-item>
-                <el-menu-item index="4">
+                <el-menu-item index="4" @click="get_article_list_for_laravel()">
                     <i class="el-icon-setting"></i>
-                    <span slot="title" @click="get_article_list_for_laravel()">Laravel</span>
+                    <span slot="title">Laravel</span>
                 </el-menu-item>
 
-                <el-menu-item index="5">
+                <el-menu-item index="5" @click="get_article_list_for_python()">
                     <i class="el-icon-s-open"></i>
-                    <span slot="title" @click="get_article_list_for_python()">Python基础</span>
+                    <span slot="title">Python基础</span>
                 </el-menu-item>
-                <el-menu-item index="6">
+                <el-menu-item index="6" @click="get_article_list_for_vue()">
                     <i class="el-icon-info"></i>
-                    <span slot="title" @click="get_article_list_for_vue()">VUE</span>
+                    <span slot="title">VUE</span>
                 </el-menu-item>
 
             </el-menu>
@@ -50,17 +51,33 @@
 <script>
     let vm;
     export default {
-        created(){
+        created() {
             vm = this
+            // 设置选中状态
+            vm.navselected = vm.index_to_path[this.$route.path]
         },
-        data(){
+        data() {
             return {
-                navselected:"1"
+                navselected: "1",
+                default_active:"1",
+                index_to_path: {
+                    "/JiJinList/JiJinAnalysize": "1",
+                    "/JiJinList/articleList/1": "2",
+                    "/JiJinList/articleList/2":"4",
+                    "/JiJinList/articleList/3" : "5",
+                   "/JiJinList/articleList/4": "6",
+                }
             }
         },
+        mounted(){
+             vm.default_active = vm.index_to_path[this.$route.path]
+        },
         methods: {
+            selectItems(index) {
+                vm.navselected = index
+            },
             handleOpen(key, keyPath) {
-                vm;
+
                 console.log(key, keyPath);
             },
             handleClose(key, keyPath) {
@@ -81,7 +98,7 @@
 
             }, get_article_list_for_vue() {
                 //location.href = "/JiJinList/articleDetail/1"  // laravel
-                   location.href = "/JiJinList/articleList/4"  // laravel
+                location.href = "/JiJinList/articleList/4"  // laravel
 
             }
 
