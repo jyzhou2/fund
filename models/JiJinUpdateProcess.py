459e6d252f3c633cefbef54b21e13dbe4e150304
dasgk <674026565@qq.com>
2020-08-20 21:36:14 +0800

    暂时提交

--- models/JiJinUpdateProcess.py
+++ models/JiJinUpdateProcess.py
@@ -1,6 +1,6 @@
 from peewee import *
 import sys
-sys.append('..')
+sys.path.append('..')
 from config.database import DATABASE_CONFIG
 database = MySQLDatabase('test', **{'charset': 'utf8', 'use_unicode': True, 'host': DATABASE_CONFIG['host'], 'user': DATABASE_CONFIG['user'], 'password': DATABASE_CONFIG['password'],'port':DATABASE_CONFIG['port']})
 
