bba20a5284fc2c02e7fd3ba38f749d563fc2add9
dasgk <674026565@qq.com>
2020-09-01 22:47:04 +0800

    ``

--- Schedule.py
+++ Schedule.py
@@ -1,9 +1,18 @@
+from events.CollectFundEvent import CollectFundEvent
+from events.DescendingStaticsEvent import DescendingStaticsEvent
+
 
 class Schedule:
     def __init__(self):
-        self.events = []
+        collect = CollectFundEvent()
+        descend = DescendingStaticsEvent()
+        self.events = [collect, descend]
 
     def handle(self):
-
         for item in self.events:
             print(item.handle())
+
+
+if __name__ == '__main__':
+    hModal = Schedule()
+    hModal.handle()
---
+++ events/DescendingStaticsEvent.py
@@ -0,0 +1,36 @@
+from DingDingMsgDao import DingDingMsgDao
+from FileCache import FileCache
+import time
+from LogDao import LogDao
+from DescendingStatics import DescnedingStatics
+
+
+class DescendingStaticsEvent:
+    def __init__(self):
+        self.runat = '14:20:00'
+        self.event_name = 'descendingstatics'
+
+    def handle(self):
+        now = time.strftime("%H:%M:%S")
+        # 大于已经要执行的时间
+        if now > self.runat:
+            # 今天尚未执行
+            if FileCache.get(self.event_name) is not None:
+                return
+            else:
+                # 开始更新基金信息
+                DescendingStaticsEvent.updateFundInfo(self.event_name)
+        else:
+            return
+
+
+    staticmethod
+    def updateFundInfo(event_name):
+        # 开始执行操作
+        LogDao.saveLog('crond', '开始统计基金跌幅信息')
+        hModal = DescnedingStatics()
+        hModal.handle()
+        msg = DingDingMsgDao()
+        msg.sendMsg('跌幅统计完成')
+        LogDao.saveLog('crond', '基金跌幅统计完成')
+        FileCache.put(event_name, 1, 3600 * 23)
